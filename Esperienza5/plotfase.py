5e16dda310f5071feb4a6f93a8e02fe7ec9b487c
MaXpanza <massimo.bilancioni@sns.it>
2018-11-10 23:42:54 +0100

    punto 3 a quasi finito

--- Esperienza5/plotfase.py
+++ Esperienza5/plotfase.py
@@ -26,12 +26,22 @@ AdB = 20* np.log10(vout/vin)
 fase = phi[f<82]
 Dadb = 0.26*np.ones(len(AdB) )
 dfase = 0.04* fase
-plt.errorbar( F,AdB,Dadb, None ,'.')
+plt.errorbar( F,fase,dfase, None ,'.')
 plt.xlabel("log f(kHz)")
 plt.ylabel("$\phi ( ^\circ)$")
 plt.xscale("log")
 plt.show()
 
+
+def h( x,a):
+    return  180 - 360/(6.28) *np.arctan(np.power(10,np.log10(x)-a))
+    
+    
+
+t = np.linspace(0.01,100,100000)
+
+
+plt.plot(t,h(t,-0.481))
 
 
 
