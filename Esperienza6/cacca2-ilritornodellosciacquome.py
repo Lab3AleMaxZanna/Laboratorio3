fa2dd1d5ea1f235f0a1c7a6a0dbe7e12261f8fab
alepholi <alessandro.foligno@sns.it>
2018-11-20 11:53:38 +0100

    parte conclusa, aggiunta di immagine del fit e di un bellerrimo script in Python

---
+++ Esperienza6/cacca2-ilritornodellosciacquome.py
@@ -0,0 +1,10 @@
+import numpy as np 
+
+
+ 
+import matplotlib.pyplot as plt 
+
+
+ 
+from scipy.optimize import curve_fit 
+np.loadtxt("C:\\Users\\alexf\OneDrive\Desktop\laboratorio\Laboratorio3\Esperienza6\nonnaLuisa.txt",unpack=True)
