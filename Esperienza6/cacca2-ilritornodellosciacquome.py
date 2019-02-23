13a7c7e0c6d1213815225e2330e1f3c0fab1adb0
alepholi <alessandro.foligno@sns.it>
2019-02-23 16:20:47 +0100

    porchiddio

--- Esperienza6/cacca2-ilritornodellosciacquome.py
+++ Esperienza6/cacca2-ilritornodellosciacquome.py
@@ -7,4 +7,17 @@ import matplotlib.pyplot as plt
 
  
 from scipy.optimize import curve_fit 
-np.loadtxt("C:\\Users\\alexf\OneDrive\Desktop\laboratorio\Laboratorio3\Esperienza6\nonnaLuisa.txt",unpack=True)
+t,dt,v,dv=np.loadtxt("C:\\Users\\alexf\OneDrive\Desktop\laboratorio\Laboratorio3\Esperienza6\\nonnaLuisa.txt",unpack=True)
+dt_eff=np.sqrt(dt**2+(dv/v*0.38)**2)
+
+def func(x,a,b):
+    return b*np.log(x/a)
+popt,pcov=curve_fit(func,v,t,sigma=dt_eff)
+print(popt,pcov)
+plt.errorbar(v,t,dt,dv,'.')
+x=np.linspace(1,10)
+plt.plot(x,func(x,popt[0],popt[1]))
+plt.xlabel("$V_{in}[V]$")
+plt.ylabel("$\Delta t[ms]$")
+plt.title("durata del segnale alto")
+plt.show()
